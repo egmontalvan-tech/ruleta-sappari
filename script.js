37927cba873ba4b3d3d1e8c40407c4e6a52f7502
egmontalvan-tech <egmontalvan@gmail.com>
2026-05-30 22:11:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,138 +1,31 @@
-const canvas = document.getElementById("wheel");
-const ctx = canvas.getContext("2d");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
-const prizes = [
-    "2x1 en bebidas",
-    "Sigue participando",
-    "8 Rollitos",
-    "Sigue participando",
-    "20% Desc. Infantil",
-    "Sigue participando",
-    "2x1 en Sopas",
-    "Sigue participando"
-];
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    where,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-const colors = [
-    "#8B0000",
-    "#000000",
-    "#8B0000",
-    "#000000",
-    "#8B0000",
-    "#000000",
-    "#8B0000",
-    "#000000"
-];
+const firebaseConfig = {
+    apiKey: "AIzaSyD4anQngP2eDrG1QCfoeSkanidlvOc6k9E",
+    authDomain: "sappari.firebaseapp.com",
+    projectId: "sappari",
+    storageBucket: "sappari.firebasestorage.app",
+    messagingSenderId: "353921307187",
+    appId: "1:353921307187:web:c475868367b1e68ee40946",
+    measurementId: "G-4X05BCVFEG"
+};
 
-const arc = (2 * Math.PI) / prizes.length;
+const app = initializeApp(firebaseConfig);
 
-function drawWheel(){
+const db = getFirestore(app);
 
-    for(let i=0;i<prizes.length;i++){
-
-        const angle = i * arc;
-
-        ctx.beginPath();
-
-        ctx.fillStyle = colors[i];
-
-        ctx.moveTo(250,250);
-
-        ctx.arc(
-            250,
-            250,
-            250,
-            angle,
-            angle + arc
-        );
-
-        ctx.fill();
-
-        ctx.save();
-
-        ctx.translate(250,250);
-
-        ctx.rotate(angle + arc/2);
-
-        ctx.fillStyle="white";
-
-        ctx.font="bold 18px Poppins";
-
-        ctx.textAlign="right";
-
-        ctx.fillText(
-            prizes[i],
-            220,
-            10
-        );
-
-        ctx.restore();
-    }
-}
-
-drawWheel();
-
-document
-.getElementById("spinBtn")
-.addEventListener("click", spinWheel);
-
-function spinWheel(){
-
-    const today = new Date().toDateString();
-
-    const lastSpin =
-    localStorage.getItem("sappari_last_spin");
-
-    if(lastSpin === today){
-
-        alert(
-            "🍣 Ya participaste hoy. Regresa mañana."
-        );
-
-        return;
-    }
-
-    localStorage.setItem(
-        "sappari_last_spin",
-        today
-    );
-
-    const winnerIndex =
-    Math.floor(
-        Math.random() * prizes.length
-    );
-
-    const anglePerPrize =
-    360 / prizes.length;
-
-    const stopAngle =
-    360 * 6 +
-    (360 - winnerIndex * anglePerPrize);
-
-    canvas.style.transition =
-    "transform 6s ease-out";
-
-    canvas.style.transform =
-    `rotate(${stopAngle}deg)`;
-
-    setTimeout(()=>{
-
-        document.getElementById(
-            "prizeText"
-        ).innerHTML =
-        prizes[winnerIndex];
-
-        document.getElementById(
-            "winnerModal"
-        ).style.display =
-        "flex";
-
-    },6000);
-}
-
-function closeModal(){
-
-    document.getElementById(
-        "winnerModal"
-    ).style.display =
-    "none";
-}
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
+window.query = query;
+window.where = where;
+window.getDocs = getDocs;
